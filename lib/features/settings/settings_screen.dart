fd4a0e9a15aea676c1caa6934bc734e766a4c93c
Piyush Orke <piyushorke059@gmail.com>
2026-09-07 20:34:28 +0530

    Remove Debug DeviceName from production

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../core/services/settings_service.dart';
 
 class SettingsScreen extends StatefulWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -99,62 +99,7 @@ class HomeScreen extends StatelessWidget {
               ),
 
               const Spacer(),
-
-              const Text(
-                'Offline • Private • Direct',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 13,
-                ),
-              ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-class DeviceIdDebugScreen extends StatefulWidget {
-  const DeviceIdDebugScreen({super.key});
-
-  @override
-  State<DeviceIdDebugScreen> createState() => _DeviceIdDebugScreenState();
-}
-
-class _DeviceIdDebugScreenState extends State<DeviceIdDebugScreen> {
-  final DeviceIdentityService _identityService =
-  DeviceIdentityService();
-
-  String _deviceId = 'Loading...';
-
-  @override
-  void initState() {
-    super.initState();
-    _loadDeviceId();
-  }
-
-  Future<void> _loadDeviceId() async {
-    final id = await _identityService.getDeviceId();
-
-    if (mounted) {
-      setState(() {
-        _deviceId = id;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Device Identity'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: SelectableText(
-          _deviceId,
-          style: const TextStyle(
-            fontSize: 16,
           ),
         ),
       ),
